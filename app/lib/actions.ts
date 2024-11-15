d29196c93c6e9e46f9a0a91953bae3dc19070c56
Lital Levin <litalonet@gmail.com>
2024-11-15 17:32:30 +0100

    update and delete actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,8 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+{/** Create new invoice */}
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
@@ -34,6 +36,31 @@ export async function createInvoice(formData: FormData) {
 
   expirePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+  }
 
+  {/** Update an invoice */}
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    expirePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    expirePath('/dashboard/invoices');
   }
